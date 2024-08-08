f933c2d56843ba507ca30ae18214c32471818006
Joao Victor Gregorio <joaogregoriodsilva@gmail.com>
2024-08-08 10:43:29 -0300

    Exercício 089 finalizado, último abordando o tópico de listas

--- Mundo3/Listas (Parte 2)/ex089.py
+++ Mundo3/Listas (Parte 2)/ex089.py
@@ -14,22 +14,34 @@ logo = red("""
 os.system("cls")
 print(logo)
 
-students = []
-
+"""Inserção de dados dos alunos e verificação de continuidade do programa"""
+token = []
 while True:
     name = str(input("\nNome: ")).strip().title()
-    students.append([name, [], []])
     note1 = float(input("Nota 1: "))
     note2 = float(input("Nota 2: "))
+    average = (note1 + note2) / 2
+    token.append([name, [note1, note2], average])
     option = str(input("Deseja continuar? [S/N]: ")).strip().upper()[0]
     if option == "N":
         break
 
-print(f"\n{yellow('No.')} {green('Nome')} {blue('Média')}")
-for i, student in enumerate(students):
+"""Mostra as informações dos alunas de forma tabular"""
+print("\n" + red("-" * 26, bold=True))
+print(f"{'No.':<4} {'NOME':<10} {'MÉDIA':>8}")
+print(red("-" * 26, bold=True))
+for i, student in enumerate(token):
     average = (student[1][0] + student[1][1]) / 2
-    print(f"{i + 1:<3} {student[0]:<10} {average:.1f}")
+    print(f"{i + 1:<4} {student[0]:<10} {student[2]:>8.1f}")
 
+"""Mostra as notas de cada aluno individualmente"""
+while True:
+    option = int(input("\nMostrar notas de qual aluno? (999 interrompe): "))
+    if option == 999:
+        break
+    if option <= len(token):
+        print(f"Notas de {token[option - 1][0]} são {token[option - 1][1]}")
+    else:
+        print(red("Opção inválida. Tente novamente.", bold=True))
 
-    
-    
+print(yellow("\nPrograma finalizado...", bold=True))
