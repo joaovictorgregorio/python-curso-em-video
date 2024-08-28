46f231f3f870e92445861c41f6fcdab7db61fa1d
Joao Victor Gregorio <joaogregoriodsilva@gmail.com>
2024-08-28 20:28:59 -0300

    Exercício 109 finalizado com sucesso

--- Mundo3/Modularizacao/ex109/main.py
+++ Mundo3/Modularizacao/ex109/main.py
@@ -4,7 +4,8 @@ import moeda
 os.system("cls")
 
 preco = float(input("Digite um preço: R$"))
-print(f"A metade de {moeda.moeda(preco)} é {moeda.moeda(moeda.metade_valor(preco, True))}")
-# print(f"O dobro de {moeda.moeda(preco)} é {moeda.moeda(moeda.dobro_valor(preco, True))}")
-# print(f"Aumentando {moeda.moeda(preco)} em 10%, temos {moeda.moeda(moeda.aumento_valor(preco, 10, False))}")
-# print(f"Reduzindo {moeda.moeda(preco)} em 13%, temos {moeda.moeda(moeda.reducao_valor(preco, 13, True))}")
+print(f"A metade de {moeda.moeda(preco)} é {moeda.metade_valor(preco)}")
+print(f"O dobro de {moeda.moeda(preco)} é {moeda.dobro_valor(preco, True)}")
+print(f"Aumentando {moeda.moeda(preco)} em 10%, temos {moeda.aumento_valor(preco, 10)}")
+print(f"Reduzindo {moeda.moeda(preco)} em 13%, temos {moeda.reducao_valor(preco, 13, True)}")
+help(moeda)
--- Mundo3/Modularizacao/ex109/moeda.py
+++ Mundo3/Modularizacao/ex109/moeda.py
@@ -1,31 +1,54 @@
-def metade_valor(preco, formatacao):
-    if formatacao:
-        return moeda(preco)
+def metade_valor(preco, formatacao=False):
+    """
+    --> Retorna a metade do preço
+    :param preco: valor a ser calculado
+    :param formatacao: (opcional) se True, retorna o valor formatado
+    :return: valor da metade do preço
+    """
     metade = preco / 2
-    return metade
+    return metade if formatacao is False else moeda(metade)
 
 
-# def dobro_valor(preco, formatacao):
-#     if formatacao:
-#         return moeda(preco)
-#     dobro = preco * 2
-#     return dobro
+def dobro_valor(preco, formatacao=False):
+    """
+    --> Retorna o dobro do preço
+    :param preco: valor a ser calculado
+    :param formatacao: (opcional) se True, retorna o valor formatado
+    :return: valor do dobro do preço
+    """
+    dobro = preco * 2
+    return dobro if formatacao is False else moeda(dobro)
 
 
-# def aumento_valor(preco, taxa, formatacao):
-#     if formatacao:
-#         return moeda(preco)
-#     aumento = preco + (preco * taxa / 100)
-#     return aumento
+def aumento_valor(preco, taxa, formatacao=False):
+    """
+    --> Retorna o valor aumentado
+    :param preco: valor a ser calculado
+    :param taxa: porcentagem do aumento
+    :param formatacao: (opcional) se True, retorna o valor formatado
+    :return: valor aumentado
+    """
+    aumento = preco + (preco * taxa / 100)
+    return aumento if formatacao is False else moeda(aumento)
 
 
-# def reducao_valor(preco, taxa, formatacao):
-#     if formatacao:
-#         return moeda(preco)
-#     reducao = preco - (preco * taxa / 100)
-#     return reducao
+def reducao_valor(preco, taxa, formatacao=False):
+    """
+    --> Retorna o valor reduzido
+    :param preco: valor a ser calculado
+    :param taxa: porcentagem do aumento
+    :param formatacao: (opcional) se True, retorna o valor formatado
+    :return: valor reduzido
+    """
+    reducao = preco - (preco * taxa / 100)
+    return reducao if formatacao is False else moeda(reducao)
 
 
 def moeda(preco):
+    """
+    --> Retorna o valor formatado
+    :param preco: valor a ser formatado
+    :return: valor formatado
+    """
     formatacao = f"R${preco:.2f}".replace(".", ",")
     return formatacao
